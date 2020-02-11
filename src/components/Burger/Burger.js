8718d004a4bc1a8a0e9568fa24f89fc6f0fd17c1
Santiago Ramos <santiago.ramos.romeu@everis.com>
2020-02-11 12:47:05 +0100

    Added Burger.module.css with media-queries to fit the burger display. Imported BurgerIngredient to display the different ingredients on the burger. Displayed the Burger component on the app.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,3 +1,16 @@
-const burguer = (props) => {
+import React from 'react';
+import classes from 'Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
+const burguer = props => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
+
+export default burger;
