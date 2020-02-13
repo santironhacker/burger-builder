fe74ea745fa6279836d611a5633809ddb45e594e
Santiago Ramos <santiago.ramos.romeu@everis.com>
2020-02-13 19:09:56 +0100

    Created individual controls at unit level for adding/removing ingredients. Created styles for the elements

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Salad', type: 'salad' },
+  { label: 'Salad', type: 'salad' },
+  { label: 'Salad', type: 'salad' },
+  { label: 'Salad', type: 'salad' }
+];
