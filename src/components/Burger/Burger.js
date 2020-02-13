d7b358c8b5251678cda3b92960a065117903edaa
Santiago Ramos <santiago.ramos.romeu@everis.com>
2020-02-13 19:10:50 +0100

    checked if there are ingredients, else output text for adding ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
   // Ingredients are passed as an object so we need to transform them to an array
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  console.log('Ingredients ', transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start addind ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
